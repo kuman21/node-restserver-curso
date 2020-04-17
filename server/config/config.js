98650faa0b2fbd0a5b378eb41db30812b750d3c8
Victor Tovar <victog21@outlook.com>
2020-04-17 11:53:14 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,7 @@ let urlDB;
 if ( process.env.NODE_ENV === 'dev' ) {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://admin:Ui5Ie5uY3sWxt3et@cluster0-vxfxq.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
